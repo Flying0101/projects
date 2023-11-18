a2545da934520a3a8826b2a1bc5e73a6bf76944c
Flying0101 <stephano@live.se>
2023-11-18 21:40:24 +0100

    Delete navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
-import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
 import ProjectOne from "@/components/ProjectsOne";
+import ProjectTwo from "@/components/ProjectsTwo";
 import { ProjectContextProvider } from "@/context/ProjectContext";
+import TechStack from "@/components/Techstack";
+import { IconContextProvider } from "@/context/IconContext";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="max-w-[1800px] mx-auto">
-      <Navigation />
       <Hero />
       <ProjectContextProvider>
         <ProjectOne />
+        <ProjectTwo />
       </ProjectContextProvider>
+      <IconContextProvider>
+        <TechStack />
+      </IconContextProvider>
+      <Footer />
     </main>
   );
 }
