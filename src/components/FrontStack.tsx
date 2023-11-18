bb8eee4dc7bc4f11a2ee1b168092b9e14d098172
Flying0101 <stephano@live.se>
2023-11-18 21:40:49 +0100

    add frontend component for icons

---
+++ src/components/FrontStack.tsx
@@ -0,0 +1,23 @@
+import { useIcons } from "@/context/IconContext";
+
+const FrontStack = () => {
+    const { frontTech }: any = useIcons();
+
+    return (
+        <div className="w-full md:w-[80%] xl:w-[60%] flex flex-wrap justify-center gap-x-14 gap-y-9">
+        {frontTech.map((icon: any) => (
+          <div className="w-full max-w-[100px] justify-center ">
+            <img
+              src={icon.img}
+              className={`${icon.type === "round" ? "rounded-[10rem]" : ""} ${
+                icon.back === "black" ? "bg-black" : ""
+              } h-auto w-full max-h-[100px]`}
+            />
+            <p className="h-fit w-full text-center">{icon.name}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+  export default FrontStack;
