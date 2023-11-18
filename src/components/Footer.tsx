2d540d02d1dc9ad46f8104006cbcfae41da953d3
Flying0101 <stephano@live.se>
2023-11-18 21:42:27 +0100

    Add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+
+
+const Footer = () => {
+    return (
+      <footer className="bg-white rounded-lg shadowx m-4 mt-24">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+          <div className="sm:flex sm:items-center sm:justify-between">
+              <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                  <img src="/sm-logo.png" className="h-8" alt="SM Logo" />
+                  <span className="self-center text-2xl font-semibold whitespace-nowrap ">Project Portfolio </span>
+              </a>
+              <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-900 sm:mb-0">
+                  <li>
+                      <a href="#" className="hover:underline me-4 md:me-6">Blog</a>
+                  </li>
+                  <li>
+                      <a href="#" className="hover:underline me-4 md:me-6">Projects</a>
+                  </li>
+                  <li>
+                      <a href="#" className="hover:underline me-4 md:me-6">Techstack</a>
+                  </li>
+              </ul>
+          </div>
+          <hr className="my-6 border-gray-200 sm:mx-auto  lg:my-8" />
+          <span className="block text-sm text-gray-900 sm:text-center">© 2023 <p className="hover:underline">Stephano Melo</p>. All Rights Reserved.</span>
+      </div>
+  </footer>
+    );
+  };
+  
+  export default Footer;
