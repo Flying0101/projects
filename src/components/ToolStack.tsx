0e5528780ff2a92bb56da8548127160759b10dd9
Flying0101 <stephano@live.se>
2023-11-22 09:38:20 +0100

    Add new component for tool tech

--- src/components/ToolStack.tsx
+++ src/components/ToolStack.tsx
@@ -1,18 +1,35 @@
 import { useIcons } from "@/context/IconContext";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
 const ToolStack = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   const { toolTech }: any = useIcons();
   return (
-    <div className="w-full md:w-[80%] xl:w-[60%] flex flex-wrap justify-center gap-x-14 gap-y-9">
+    <div
+      className="w-full md:w-[80%] xl:w-[60%] flex flex-wrap justify-center gap-x-14 gap-y-9"
+      ref={ref}
+    >
       {toolTech.map((icon: any) => (
-        <div className="w-full max-w-[100px] justify-center ">
-          <img
-            src={icon.img}
-            className={`${icon.type === "round" ? "rounded-[10rem]" : ""} ${
-              icon.back === "black" ? "bg-black" : ""
-            } h-auto w-full max-h-[100px]`}
-          />
-          <p className="h-fit w-full text-center">{icon.name}</p>
-        </div>
+        <span
+        key={icon.id}
+          style={{
+            transform: isInView ? "none" : "translateX(-300px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.1s",
+          }}
+        >
+          <div  className="w-full max-w-[100px] justify-center ">
+            <img
+              src={icon.img}
+              className={`${icon.type === "round" ? "rounded-[10rem]" : ""} ${
+                icon.back === "black" ? "bg-black" : ""
+              } mx-auto h-auto w-auto max-h-[60px] md:max-h-[100px]`}
+            />
+            <p className="h-fit w-full text-center">{icon.name}</p>
+          </div>
+        </span>
       ))}
     </div>
   );
