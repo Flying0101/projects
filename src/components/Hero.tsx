421f0b239d017919ebb9c5736236b5bd93fe8293
Flying0101 <stephano@live.se>
2023-11-22 21:55:23 +0100

    add new functionality for text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,10 @@
-
 const Hero = () => {
+  const heroText = "Welcome To My Projects";
+  const heroInfo =
+    ' Press the  "Explore more" button to see my different projects showcasing different types of custom developed web solutions';
+
+  const letters = heroText.split("");
+  const infoLetters = heroInfo.split("");
 
   return (
     <div className="bg-white min-h-90vh] " id="hero">
@@ -19,11 +24,24 @@ const Hero = () => {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Welcome To My Projects
+              {letters.map((letter, index) => (
+                <span
+                  key={index}
+                  className="hover:text-5xl transition-all duration-100 ease-in"
+                >
+                  {letter}
+                </span>
+              ))}
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Press the &#34Explore more&#34 button to see my different projects
-              showcasing different types of custom developed web solutions.
+              {infoLetters.map((letter, index) => (
+                <span
+                  key={index}
+                  className="hover:text-xl transition-all duration-100 ease-in"
+                >
+                  {letter === '"' ? <>&quot;</> : letter}
+                </span>
+              ))}
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
