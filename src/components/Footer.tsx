53ce00c35bf62e362287735f825d5c0dcc6147d1
Flying0101 <stephano@live.se>
2023-11-22 21:55:12 +0100

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,13 @@
 'use client'
 import scrollTo from "@/utils/ScrollTo";
-
 const Footer = () => {
     return (
       <footer className="bg-white rounded-lg shadowx m-4 mt-24">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
-              <div  className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse hover:cursor-pointer" onClick={()=>scrollTo('hero')}>
-                  <img src="/sm-logo.png" className="h-8" alt="SM Logo" />
-                  <span className="self-center text-2xl font-semibold whitespace-nowrap ">Project Portfolio </span>
+              <div  className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse hover:cursor-pointer" >
+                  <img src="/sm-logo.png" className="h-8" alt="SM Logo" onClick={()=>scrollTo('hero')} />
+                  <span className="self-center text-2xl font-semibold whitespace-nowrap " onClick={()=>scrollTo('project')}>Project Portfolio </span>
               </div>
               <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-900 sm:mb-0">
                   <li>
