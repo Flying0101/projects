0af8907074cb1b30eb98345ede3fe253c4b9a92a
Flying0101 <stephano@live.se>
2023-11-22 09:37:37 +0100

    update css and links

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,8 @@
+
 const Hero = () => {
+
   return (
-    <div className="bg-white max-h-[90vh]">
+    <div className="bg-white min-h-90vh] " id="hero">
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -15,22 +17,21 @@ const Hero = () => {
           ></div>
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            The Full-Stack Story Of My Projects Through Time
+              Welcome To My Projects
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-             Press the "Explore more" button to see my different projects showcasing different types of custom developed web solutions.
+              Press the &#34Explore more&#34 button to see my different projects
+              showcasing different types of custom developed web solutions.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="#"
+                href="#project"
                 className="rounded-md bg-lighterSecondary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
               >
                 Explore more
               </a>
-             
             </div>
           </div>
         </div>
