c7b31237c3e1fc53fb4f3e871a05c0accb5ea224
Flying0101 <stephano@live.se>
2023-11-22 09:37:13 +0100

    add functionality to footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,24 @@
-
+'use client'
+import scrollTo from "@/utils/ScrollTo";
 
 const Footer = () => {
     return (
       <footer className="bg-white rounded-lg shadowx m-4 mt-24">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
-              <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+              <div  className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse hover:cursor-pointer" onClick={()=>scrollTo('hero')}>
                   <img src="/sm-logo.png" className="h-8" alt="SM Logo" />
                   <span className="self-center text-2xl font-semibold whitespace-nowrap ">Project Portfolio </span>
-              </a>
+              </div>
               <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-900 sm:mb-0">
                   <li>
-                      <a href="#" className="hover:underline me-4 md:me-6">Blog</a>
+                      <a href="/404" className="hover:underline me-4 md:me-6">Blog</a>
                   </li>
                   <li>
-                      <a href="#" className="hover:underline me-4 md:me-6">Projects</a>
+                      <a href="#project" className="hover:underline me-4 md:me-6">Projects</a>
                   </li>
                   <li>
-                      <a href="#" className="hover:underline me-4 md:me-6">Techstack</a>
+                      <a href="#tech" className="hover:underline me-4 md:me-6">Techstack</a>
                   </li>
               </ul>
           </div>
