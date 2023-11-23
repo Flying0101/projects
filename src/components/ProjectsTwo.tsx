0b1a19ddd1b5291cc3b6445b1bb6a891aa1a2d10
Flying0101 <stephano@live.se>
2023-11-23 21:24:54 +0100

    update css

--- src/components/ProjectsTwo.tsx
+++ src/components/ProjectsTwo.tsx
@@ -40,7 +40,7 @@ const ProjectTwo = () => {
                       </time>
                       <a
                         href="#"
-                        className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                        className="relative z-10 rounded-full mb-2 bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
                       >
                         {file.type}
                       </a>
