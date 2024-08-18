632abb49286a487cf3730b468ec50694002f2534
Flying0101 <stephano@live.se>
2024-08-18 21:31:24 +0200

    update copy

--- src/components/ProjectsOne.tsx
+++ src/components/ProjectsOne.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useProjects } from "@/context/ProjectContext";
-import Link from "next/link";
 const ProjectOne = () => {
   const { fullProjects }: any = useProjects();
 
@@ -10,10 +9,10 @@ const ProjectOne = () => {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              Full-Stack Projects
+              — Headless CMS Projects
             </h2>
             <p className="mt-2 text-lg leading-8 text-gray-600">
-              Check my work of web solutions connected to headless cms.
+              Check out my web solutions connected to headless CMS.
             </p>
           </div>
           <div className="relative mt-8 flex items-center gap-x-4">
@@ -24,12 +23,10 @@ const ProjectOne = () => {
             />
             <div className="text-sm leading-6">
               <p className="font-semibold text-gray-900">
-                <a href="#">
-                  <span className="absolute inset-0"></span>
-                  Stephano Melo
-                </a>
+                <span className="absolute inset-0"></span>
+                Stephano Melo
               </p>
-              <p className="text-gray-600">Frontend Developer / 2 year +</p>
+              <p className="text-gray-600">Full-stack Developer / 2 year +</p>
             </div>
           </div>
           <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
