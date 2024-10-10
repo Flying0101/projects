42bad64b5d0215de1b39e82578c0437c66df82f9
Stephano <stephano@live.se>
2024-10-10 15:55:20 +0200

    fix: syntax error

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -52,13 +52,13 @@ const Navigation = () => {
               className="h-8 w-8"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               aria-hidden="true"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
               />
             </svg>
@@ -113,13 +113,13 @@ const Navigation = () => {
                 className="h-8 w-8"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 aria-hidden="true"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M6 18L18 6M6 6l12 12"
                 />
               </svg>
